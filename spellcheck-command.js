e90c2fd4fb1a97ada74563682fce322dd89592bb
Leandro Hoffmann <leh.hoff@gmail.com>
2023-10-25 11:32:18 +0100

    Add changes from call

--- spellcheck-command.js
+++ spellcheck-command.js
@@ -2,18 +2,51 @@ import { Command } from '@ckeditor/ckeditor5-core';
 
 export default class SpellcheckCommand extends Command {
   execute(dictionary) {
-    const range = this.editor.editing.view.createRangeIn(this.editor.editing.view.document.getRoot());
-    const toBeChanged = [];
-    for (const value of range.getWalker({ignoreElementEnd: true})) {
-      if (value.item.name === 'div' && value.item.hasClass('body-section')) {
-        toBeChanged.push(value.item);
+    const range = this.editor.model.createRangeIn(this.editor.model.document.getRoot());
+    const bodySections = [];
+    for (const value of range.getItems()) {
+      if (value.is('element', 'sectionContent')) {
+        bodySections.push(value);
       }
     }
-    this.editor.editing.view.change(writer => {
-      for (const viewElement of toBeChanged) {
-        this.traverseModel(viewElement, writer, dictionary);
+
+    this.editor.model.change(writer => {
+      for (const section of bodySections) {
+        const sectionRange = this.editor.model.createRangeIn(section);
+        const ranges = [];
+        for (const item of sectionRange.getItems()) {
+          if (item.is('$textProxy')) {
+            console.log(item.data);
+            let words = item.data.split(' ');
+            for (const word of words) {
+              let sIdx = item.textNode.data.indexOf(word);
+              let eIdx = sIdx + word.length;
+              const startPosition = writer.createPositionAt(item.textNode.parent, sIdx + item.textNode.startOffset);
+              const endPosition = writer.createPositionAt(item.textNode.parent, eIdx + item.textNode.startOffset);
+              const range = writer.createRange(startPosition, endPosition);
+              ranges.push(range);
+              // writer.setAttribute('words', 'test,qwe,qwe,ewq', range);
+            }
+          }
+        }
+        for (const range of ranges.reverse()) {
+          writer.setAttribute('words', 'test,qwe,qwe,ewq', range);
+        }
       }
     });
+
+    // const range = this.editor.editing.view.createRangeIn(this.editor.editing.view.document.getRoot());
+    // const toBeChanged = [];
+    // for (const value of range.getWalker({ignoreElementEnd: true})) {
+    //   if (value.item.name === 'div' && value.item.hasClass('body-section')) {
+    //     toBeChanged.push(value.item);
+    //   }
+    // }
+    // this.editor.editing.view.change(writer => {
+    //   for (const viewElement of toBeChanged) {
+    //     this.traverseModel(viewElement, writer, dictionary);
+    //   }
+    // });
 
     // for (const viewElement of toBeChanged) {
     //   if (viewElement.hasClass('highlight-selected')) {
